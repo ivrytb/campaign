713643f98357a2af52ab75e369e3221eb8cbe41f
ivrytb <ytb046@gmail.com>
2026-01-29 04:55:08 +0200

    Update matrim.js

--- api/matrim.js
+++ api/matrim.js
@@ -1,18 +1,20 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+    // שליפת הנתונים שימות המשיח שולחים
     const apiData = req.query.ApiData || "";
-    const step = req.query.step || "init";
+    // וודא שאנחנו מקבלים את ה-step מה-URL או מה-append
+    const step = req.query.step || "0"; 
 
     try {
-        // הגדרות ה-Read שלך - ניקוי מוחלט של פסיקים מהטקסט בהמשך
+        // הגדרות ה-Read שלך
         const readSettings = "ApiData,,7,,7,NO,,,,,,,,no";
 
-        // 1. כניסה ראשונה (משלוחת האב)
-        if (apiData === '' && step === "init") {
-            const firstAsk = "נא הקישו את מספר המתרים וסולמית לחזרה הקישו כוכבית";
+        // 1. כניסה ראשונה לשלוחה (step=0 ואין עדיין הקשה)
+        if (apiData === '' && step === "0") {
+            const welcomeMsg = "נא הקישו את מספר המתרים מנדרים פלוס ולסיום הקישו סולמית לחזרה לתפריט הראשי הקישו כוכבית";
             res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-            return res.send(`read=t-${firstAsk}=${readSettings}&api_link_append=step=1`);
+            return res.send(`read=t-${welcomeMsg}=${readSettings}&api_link_append=step=1`);
         }
 
         // 2. חזרה לשלוחת האב (הקשת *)
@@ -20,12 +22,15 @@ module.exports = async (req, res) => {
             return res.send(`go_to_folder=..`);
         }
 
-        // 3. טיפול בשתיקה (ApiData ריק)
+        // 3. טיפול בשתיקה (המשתמש לא הקיש כלום ועברו 7 שניות)
         if (apiData === '') {
             if (step === "2") {
-                return res.send(`go_to_folder=..`); 
+                // שתיקה שנייה ברצף - חוזרים למעלה
+                return res.send(`go_to_folder=..`);
             }
-            const reminder = "המערכת ממתינה למספר מתרים או כוכבית לחזרה";
+            // שתיקה ראשונה - נותנים תזכורת
+            const reminder = "המערכת ממתינה להקשת מספר מתרים או כוכבית לחזרה";
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
             return res.send(`read=t-${reminder}=${readSettings}&api_link_append=step=2`);
         }
 
@@ -34,24 +39,26 @@ module.exports = async (req, res) => {
         const matrim = response.data.find(m => m.Id.toString().trim() === apiData);
 
         if (!matrim) {
-            const errorMsg = `מתרים מספר ${apiData} לא נמצא הקישו שוב וסולמית`;
+            const errorMsg = `מתרים מספר ${apiData} לא נמצא נא הקישו שוב מספר מתרים וסולמית`;
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
             return res.send(`read=t-${errorMsg}=${readSettings}&api_link_append=step=1`);
         }
 
-        // 5. ניקוי הטקסט - קריטי! הסרת פסיקים וגרשיים כדי לא לשבור את ה-read
+        // 5. נמצא מתרים - הכנת הודעה מפורטת
         let name = matrim.Name.replace(/[,"']/g, '').replace(/[^א-ת ]/g, '');
         const total = Math.floor(parseFloat(matrim.Cumule));
         const donors = matrim.Donator;
         const goal = parseInt(matrim.Goal);
         const percent = goal > 0 ? Math.floor((total / goal) * 100) : 0;
 
-        // בניית המשפט ללא פסיקים בכלל
-        const textToSay = `${name} השיג ${percent} אחוזים והתרים ${total} שקלים מ-${donors} תורמים להקשת מתרים נוסף הקישו מספר וסולמית`;
+        // בניית משפט ברור ללא פסיקים
+        const info = `${name} השיג ${percent} אחוז מהיעד והתרים ${total} שקלים באמצעות ${donors} תורמים`;
+        const footer = "לבחירת מתרים אחר הקישו את המספר וסולמית או כוכבית לחזרה";
 
         res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-        return res.send(`read=t-${textToSay}=${readSettings}&api_link_append=step=1`);
+        return res.send(`read=t-${info} ${footer}=${readSettings}&api_link_append=step=1`);
 
     } catch (error) {
-        return res.send(`id_list_message=t-חלה שגיאה&go_to_folder=..`);
+        return res.send(`id_list_message=t-חלה שגיאה במערכת&go_to_folder=..`);
     }
 };
